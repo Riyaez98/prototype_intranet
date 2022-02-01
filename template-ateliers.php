3d43e2f4d4919991c2e348a18fd6514231b3ffe6
Ricardo Yanez <riyaez@gmail.com>
2022-02-01 15:27:51 -0500

    Creation tmplt ateliers

---
+++ template-ateliers.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Page ateliers*/
+?>
+
+<?php get_header(); ?>
+
+<main class="wrapper">
+    
+<h1>    <?php the_title() ;?>  </h1>
+<?php the_content();?>
+
+</main>
+
+<?php get_footer(); ?>
