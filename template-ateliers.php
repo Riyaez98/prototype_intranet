d6e6e31e1bc80b489b9e321bbefcea13dee24b8a
Ricardo Yanez <riyaez@gmail.com>
2022-02-01 16:05:11 -0500

    Affiche ateliers dans page

--- template-ateliers.php
+++ template-ateliers.php
@@ -4,11 +4,25 @@
 
 <?php get_header(); ?>
 
-<main class="wrapper">
-    
-<h1>    <?php the_title() ;?>  </h1>
-<?php the_content();?>
+    <main class="wrapper">
+        
+    <h1>    <?php the_title() ;?>  </h1>
 
+    <?php
+    query_posts(array(
+    'post_type' => 'evenements',
+    'post_status' => 'publish'
+    )); ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <div>
+            <div>
+                <h3><?php the_title(); ?></h3>
+                
+            </div>
+        </div>
+        <?php endwhile; ?>
+
+    <?php wp_reset_query(); ?>
 </main>
 
 <?php get_footer(); ?>
