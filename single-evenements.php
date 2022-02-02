7419d61086089ca57dfe8766f536657606e03d55
Ricardo Yanez <riyaez@gmail.com>
2022-02-02 15:30:36 -0500

    Test afficher valeurs d l'evenement fonctionnel

--- single-evenements.php
+++ single-evenements.php
@@ -5,6 +5,15 @@
     <?php component('calendar'); ?>
 
     <h1><?php the_title(); ?></h1>
+    <div class="info-atelier">
+        <div class="info-atelier__item info-atelier__date"><strong>Date : </strong><?php the_field('event_date');?></div>
+        <div class="info-atelier__item info-atelier__start"><strong>Heure début : </strong><?php the_field('event_starttime');?></div>
+        <div class="info-atelier__item info-atelier__end"><strong>Heure fin : </strong><?php the_field('event_endtime');?></div>
+        <div class="info-atelier__item info-atelier__mod"><strong>Modératrice : </strong><?php the_field('event_teacher');?></div>
+        <div class="info-atelier__item info-atelier__partner"><strong>Partenaire : </strong><?php the_field('event_partner');?></div>
+        <div class="info-atelier__item info-atelier__description"><strong>Description : </strong><?php the_field('event_description');?></div>
+    </div>
+    <!-- https://www.w3schools.com/tags/att_input_value.asp  predefine value input -->
 
 </main>
 
