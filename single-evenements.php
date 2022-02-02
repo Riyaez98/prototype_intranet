467944bfdb839c64ffd3811f4e8c3eb7855c0573
Ricardo Yanez <riyaez@gmail.com>
2022-02-02 16:03:50 -0500

    Info s'affiche dans des input, reste a enregistrer

--- single-evenements.php
+++ single-evenements.php
@@ -2,18 +2,46 @@
 
 <main class="wrapper">
     <!-- test push 02-02-2022 9h -->
-    <?php component('calendar'); ?>
 
     <h1><?php the_title(); ?></h1>
-    <div class="info-atelier">
-        <div class="info-atelier__item info-atelier__date"><strong>Date : </strong><?php the_field('event_date');?></div>
-        <div class="info-atelier__item info-atelier__start"><strong>Heure début : </strong><?php the_field('event_starttime');?></div>
-        <div class="info-atelier__item info-atelier__end"><strong>Heure fin : </strong><?php the_field('event_endtime');?></div>
-        <div class="info-atelier__item info-atelier__mod"><strong>Modératrice : </strong><?php the_field('event_teacher');?></div>
-        <div class="info-atelier__item info-atelier__partner"><strong>Partenaire : </strong><?php the_field('event_partner');?></div>
-        <div class="info-atelier__item info-atelier__description"><strong>Description : </strong><?php the_field('event_description');?></div>
-    </div>
+    
     <!-- https://www.w3schools.com/tags/att_input_value.asp  predefine value input -->
+    <form action="/action_page.php" class="info-atelier">
+
+        <div class="info-atelier__item info-atelier__date">
+            <label for="event-date"> <strong> Date : </strong> </label>
+            <input type="date" id="event-date" name="event-date" 
+                value="<?php the_field('event_date');?>" min="2020-01-01" max="2050-12-31">
+        </div>     
+
+        <div class="info-atelier__item info-atelier__start">
+            <label for="starttime"> <strong> Heure début : </strong></label>
+            <input type="time" id="starttime" name="starttime" min="00:00" max="23:59"
+                value="<?php the_field('event_starttime');?>">
+        </div>      
+
+        <div class="info-atelier__item info-atelier__end">
+            <label for="endtime"> <strong> Heure fin : </strong></label>
+            <input type="time" id="endtime" name="endtime" min="00:00" max="23:59"
+                value="<?php the_field('event_endtime');?>">
+        </div>   
+
+        <div class="info-atelier__item info-atelier__mod">
+            <label for="moderatrice"> <strong> Modératrice : </strong></label>
+            <input type="text" id="moderatrice" name="moderatrice" value="<?php the_field('event_teacher');?>">
+        </div> 
+
+        <div class="info-atelier__item info-atelier__partner">
+        <label for="partner"> <strong> Partenaire : </strong></label>
+            <input type="text" id="partner" name="partner" value="<?php the_field('event_partner');?>">
+        </div>  
+
+        <div class="info-atelier__item info-atelier__description">
+            <label for="description"> <strong> Description : </strong></label>
+                <textarea class="input__element u-grid-fullwidth" id="description" name="description" rows="6" cols="25"><?php echo the_field('event_description');?></textarea>
+        </div>        
+        <input type="submit" value="Submit">
+    </form>
 
 </main>
 
