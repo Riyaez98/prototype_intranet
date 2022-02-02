1b24e947d7ed3b3e0a44eba30519c9d2d56c4084
Ricardo Yanez <riyaez@gmail.com>
2022-02-02 12:11:31 -0500

    Test relation marche pas

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ include(__DIR__ . '/config/config-cpt.php');
 include(__DIR__ . '/config/config-menu.php');
 include(__DIR__ . '/config/config-redirect.php');
 include(__DIR__ . '/config/config-autofill_form.php');
-include(__DIR__ . '/config/config-relation_apc.php');
 
 function component($component_name) {
 
