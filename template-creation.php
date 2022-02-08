da0739d0d3014d4806d50e5dc7115912a9817376
Ricardo Yanez <riyaez@gmail.com>
2022-02-08 10:18:45 -0500

    Add cpt moderatrice et tmpl creation

---
+++ template-creation.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Creation*/
+?>
+<?php get_header(); ?>
+
+<main class="wrapper wrapper__creation">
+    
+<h1>    <?php the_title() ;?>  </h1>
+
+<?php the_content();?>
+
+</main>
+
+<?php get_footer(); ?>
